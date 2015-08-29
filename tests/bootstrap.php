324c5161dc6bb1aa809ce793c5d658fa1e1645df
marcaube <marcaube@users.noreply.github.com>
2015-08-29 15:54:23 -0400

    Update tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,7 @@
 
 if (!file_exists($autoloadFile = __DIR__ . '/../vendor/autoload.php') &&
     !file_exists($autoloadFile = __DIR__ . '/../../../vendor/autoload.php')) {
-    die('You must set up the project dependencies, run the following commands:' . PHP_EOL .
-        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
-        'php composer.phar install --dev' . PHP_EOL);
+    die('You must set up the project dependencies, run composer install' . PHP_EOL);
 }
 
 require $autoloadFile;
